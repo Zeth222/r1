349798c65a9bcac638ba1bf78a0d47f0e9005fe4
Zeth222 <jose.caobianco2@gmail.com>
2025-08-16 16:02:00 -0300

    feat: check for subgraph url

--- run.sh
+++ run.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+
+if [ -z "$UNISWAP_SUBGRAPH_URL" ]; then
+  echo "Error: UNISWAP_SUBGRAPH_URL is not set. Please set it before running this script."
+  exit 1
+fi
+
 python -m bot.main
